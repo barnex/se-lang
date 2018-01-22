3bb2e8cd9769bdfd5a4891ffe37033835c835556
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2018-01-22 20:12:05 +0100

    allow program to be a block, not just lambda

--- eva/machine.go
+++ eva/machine.go
@@ -1,7 +1,5 @@
 package eva
 
-import "fmt"
-
 type Machine struct {
 	s  []Value
 	ra Value
@@ -13,36 +11,36 @@ func (m *Machine) SP() int {
 }
 
 func (m *Machine) Push(v Value) {
-	fmt.Println("push", v)
+	Log("push", v)
 	m.s = append(m.s, v)
 }
 
 func (m *Machine) Pop() Value {
 	v := m.s[len(m.s)-1]
-	fmt.Println("pop", v)
+	Log("pop", v)
 	m.s = m.s[:len(m.s)-1]
 	return v
 }
 
 func (m *Machine) FromBP(delta int) Value {
 	v := m.s[m.bp+delta]
-	fmt.Printf("fromBP %v=%v \n", delta, v)
+	Log("fromBP", delta, "=", v)
 	return v
 }
 
 func (m *Machine) SetFromBP(delta int, v Value) {
 	m.s[m.bp+delta] = v
-	fmt.Printf("setfromBP %v to %v \n", delta, v)
+	Log("setfromBP", delta, "to", v)
 }
 
 func (m *Machine) FromSP(delta int) Value {
 	v := m.s[m.SP()+delta]
-	fmt.Printf("fromSP %v=%v \n", delta, v)
+	Log("fromSP", delta, "=", v)
 	return v
 }
 
 func (m *Machine) SetRA(v Value) {
-	fmt.Println("ra=", v)
+	Log("ra=", v)
 	m.ra = v
 }
 
@@ -59,10 +57,14 @@ func (m *Machine) SetBP(bp int) {
 }
 
 func (m *Machine) Grow(delta int) {
-	fmt.Println("grow", delta)
+	Log("grow", delta)
 	new := len(m.s) + delta
 	if new >= cap(m.s) {
 		m.s = append(m.s, make([]Value, 1+new-cap(m.s))...)
 	}
 	m.s = m.s[:new]
+}
+
+func Log(x ...interface{}) {
+	// fmt.Println(x...)
 }
--- eva/util.go
+++ eva/util.go
@@ -35,6 +35,14 @@ func CompileAST(root ast.Node) (_ Prog, err error) {
 	//	}
 	//}()
 
+	// wrap a {block} in a labmda call (()->{block})()
+	// to provide a call frame for local variables.
+	// TODO: this is a hack
+	if _, ok := root.(*ast.Block); ok {
+		root = &ast.Call{
+			F: &ast.Lambda{Body: root},
+		}
+	}
 	ast.Resolve(root)
 	return compileExpr(root), nil
 }
